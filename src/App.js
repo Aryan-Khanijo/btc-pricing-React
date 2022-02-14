1d6f2feee742bede101ee117d05e3c1dbb7c90a4
Aryan-Khanijo <akhanijo12345@gmail.com>
2022-02-14 17:16:13 +0530

    h

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
       <div className="app">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Devil Coins</a>
+            <a class="navbar-brand" href="/">Devil Coins</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
